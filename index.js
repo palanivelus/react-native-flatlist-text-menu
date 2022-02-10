ef0fa6ec04f1a51394047323c1d4f67b9b048e27
Palanivelu Samudi <cspalni@gmail.com>
2022-02-10 14:20:57 +0530

    customize the component

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 import React, { useEffect, useState } from 'react'
-import {Text,View,StyleSheet,Picker, TouchableWithoutFeedback, Alert,ToastAndroid} from 'react-native';
+import {Text,View,StyleSheet,Picker, TouchableOpacity, Alert,ToastAndroid} from 'react-native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 
 import Menu, { MenuItem, MenuDivider } from 'react-native-material-menu';
 
-const FlatListMenu=({defaultItem,selectedColor,unselectedColor,property,items,fontSize})=>{
-  console.log("delfault value",defaultItem)
+const FlatListMenu=({ defaultItem,
+  selectedColor,
+  unselectedColor,
+  property,
+  items,
+  fontSize,
+  onCancelCallback,
+  onRescheduleCallback})=>{
+  //console.log("delfault value",defaultItem)
   const [selectedValue, setSelectedValue] = useState(defaultItem);
   let _menu=null
-  const hideMenu = (title) => {
+  const hideMenu = (title, item) => {
+
+    if(title=='Reschedule'){
+      onRescheduleCallback(item);
+    }
+    else{
+      onCancelCallback(item);
+    }
+ 
       if(title){
         setSelectedValue(title)
       }
@@ -20,22 +35,22 @@ const FlatListMenu=({defaultItem,selectedColor,unselectedColor,property,items,fo
     _menu.show();
   };
     return <Menu
-          ref={(ref)=>{_menu=ref}}
-          style={{width:200,paddingTop:20}}
-          animationDuration={0}
-          button={
-         <TouchableWithoutFeedback onPress={showMenu}> 
-             <View style={{flexDirection:'row',alignItems:'center',justifyContent:'center'}}>
-             <Text style={{fontSize:16}}>{selectedValue}  </Text> 
-             </View>
-        </TouchableWithoutFeedback>}
-        >
-          {
-              items.map((item,i)=>{
-                  return <MenuItem key={i} onPress={()=>{hideMenu(item[property])}}><Text style={{color:selectedValue===item[property]?selectedColor:unselectedColor,fontSize:fontSize}}>{item[property]}</Text></MenuItem>
-              })
-            }
-        </Menu>
+    ref={(ref)=>{_menu=ref}}
+    style={{width:170,borderRadius:10}}
+    animationDuration={0}
+    button={
+   <TouchableOpacity onPress={showMenu} style={{width:50,height:60,}}> 
+       <View style={{flexDirection:'row',justifyContent:'flex-end'}}>
+       <Text style={{fontSize:30, fontWeight:'bold',paddingRight:10, paddingLeft:10,color:'#787878'}}>...</Text> 
+       </View>
+  </TouchableOpacity>}
+  >
+    {
+        items.map((item,i)=>{
+            return <MenuItem key={i} onPress={()=>{hideMenu(item['title'],item['value'])}}><Text style={{color:selectedValue===item[property]?selectedColor:unselectedColor,fontSize:fontSize}}>{item[property]}</Text></MenuItem>
+        })
+      }
+  </Menu>
 }
 
 
